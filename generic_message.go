04daf6c7e4f5e836a1044fdbd7ee360e9e3bf13f
Austin Moody <austin.moody@gmail.com>
2024-02-09 23:09:32 -0500

    Add GenericMessage struct and improve error handling
    Introduced a new GenericMessage struct to facilitate communication of generic messages. Upgraded the version of 'austinapi_db' dependency from v0.0.7 to v0.0.8. Enhanced the error handling in 'GetSleep' function, now it provides more verbose logging and proper HTTP responses. Several commented code lines also hint at future improvements.

---
+++ generic_message.go
@@ -0,0 +1,5 @@
+package main
+
+type GenericMessage struct {
+	Message string `json:"message"`
+}
--- sleep.go
+++ sleep.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"github.com/austinmoody/austinapi_db/austinapi_db"
 	"github.com/google/uuid"
 	"github.com/jackc/pgx/v5"
@@ -15,6 +16,10 @@ import (
 var (
 	SleepRegex       = regexp.MustCompile(`^/sleep/*$`)
 	SleepRegexWithID = regexp.MustCompile(`^/sleep/([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})$`)
+
+	//LogBuffer bytes.Buffer
+	//InfoLog   *log.Logger
+	//ErrorLog  *log.Logger
 )
 
 type SleepHandler struct{}
@@ -25,7 +30,13 @@ type SleepList struct {
 	PreviousToken *string              `json:"previous_token"`
 }
 
+//func init() {
+//	InfoLog = log.New(&LogBuffer, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
+//	ErrorLog = log.New(&LogBuffer, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
+//}
+
 func (h *SleepHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	log.Println("Test....")
 	switch {
 	case r.Method == http.MethodGet && SleepRegex.MatchString(r.URL.Path):
 		h.ListSleep(w, r)
@@ -37,7 +48,9 @@ func (h *SleepHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func (h *SleepHandler) GetSleep(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
 
+	// TODO - when we have caught an err internal server error and return
 	sleepIdMatches := SleepRegexWithID.FindStringSubmatch(r.URL.Path)
 
 	if len(sleepIdMatches) < 2 {
@@ -50,7 +63,12 @@ func (h *SleepHandler) GetSleep(w http.ResponseWriter, r *http.Request) {
 
 	conn, err := pgx.Connect(ctx, connStr)
 	if err != nil {
-		log.Fatalf("DB Connection error: %v", err)
+		log.Printf("DB Connection error: %v", err)
+		fmt.Printf("DB Connection error: %v", err)
+		//ErrorLog.Printf("DB Connection error: %v", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write(nil)
+		return
 	}
 	defer conn.Close(ctx)
 
@@ -58,22 +76,36 @@ func (h *SleepHandler) GetSleep(w http.ResponseWriter, r *http.Request) {
 
 	sleepUuid, err := uuid.Parse(sleepIdMatches[1])
 	if err != nil {
-		log.Fatalf("Error getting UUID: %v", sleepUuid)
+		log.Printf("error parsing UUID from http request: %v", err)
+		//ErrorLog.Printf("error parsing UUID from http request: %v", err)
 	}
 
-	mySleep, err := apiDb.GetSleep(ctx, sleepUuid)
-	// TODO not fatal error
+	getSleepResult, err := apiDb.GetSleep(ctx, sleepUuid)
+
 	if err != nil {
-		log.Fatalf("Error getting Sleep with ID %v", mySleep)
+		log.Printf("error getting sleep with uuid '%s': %v", sleepUuid.String(), err)
+		//ErrorLog.Printf("error getting sleep with uuid '%s': %v", sleepUuid.String(), err)
 	}
 
-	jsonBytes, err := json.Marshal(mySleep)
+	var result any
+
+	if len(getSleepResult) != 1 {
+		result = GenericMessage{
+			Message: "Not Found",
+		}
+		w.WriteHeader(http.StatusNotFound)
+	} else {
+		result = getSleepResult[0]
+		w.WriteHeader(http.StatusOK)
+	}
+
+	jsonBytes, err := json.Marshal(result)
 	if err != nil {
-		log.Fatalf("%v", err)
+		log.Printf("error marshaling JSON response: %v", err)
+		//ErrorLog.Printf("error marshaling JSON response: %v", err)
+		w.WriteHeader(http.StatusInternalServerError)
 	}
 
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
 	w.Write(jsonBytes)
 }
 
