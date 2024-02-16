4018a0cc63c1d7c02e6f92589af7a582b8040697
Austin Moody <austin.moody@gmail.com>
2024-02-15 21:39:10 -0500

    Update Sleeps structure and enhance Sleep data conversion
    Revised the Sleeps structure to include time and additional sleep related parameters for improvement in tracking sleep statistics. Enhanced data conversion from array of SleepsRow to Sleep format to improve flexibility of data handling. Updated the GetAllSleep API function accordingly to implement this change in the data structure.

--- sleep.go
+++ sleep.go
@@ -10,7 +10,11 @@ import (
 	"net/http"
 	"os"
 	"regexp"
+	"time"
 )
+
+// TODO - too much converting going on just to get a sqid instead of the numeric database id, rethink this
+//        has to be something simpler.
 
 var (
 	SleepRgxId   *regexp.Regexp
@@ -22,11 +26,60 @@ var (
 
 type SleepHandler struct{}
 
-// TODO custom Marshal to sqid id's
 type Sleeps struct {
-	Data          []austinapi_db.SleepsRow `json:"data"`
-	NextToken     string                   `json:"next_token"`
-	PreviousToken string                   `json:"previous_token"`
+	Data          []Sleep `json:"data"`
+	NextToken     string  `json:"next_token"`
+	PreviousToken string  `json:"previous_token"`
+}
+
+type Sleep struct {
+	ID               string
+	Date             time.Time
+	Rating           int64
+	TotalSleep       int
+	DeepSleep        int
+	LightSleep       int
+	RemSleep         int
+	CreatedTimestamp time.Time
+	UpdatedTimestamp time.Time
+}
+
+func ConvertSleepsRow(rows []austinapi_db.SleepsRow) []Sleep {
+	var sleeps []Sleep
+
+	for _, row := range rows {
+		sleep := Sleep{}
+		sleep.PopulateFromDbSleepRow(row)
+		sleeps = append(sleeps, sleep)
+	}
+
+	return sleeps
+}
+
+func (s *Sleep) PopulateFromDbSleepRow(row austinapi_db.SleepsRow) {
+
+	// TODO - handle potential errors from Encode
+	s.ID, _ = IdHasher.Encode([]uint64{uint64(row.ID)})
+	s.Date = row.Date
+	s.Rating = row.Rating
+	s.TotalSleep = row.TotalSleep
+	s.DeepSleep = row.DeepSleep
+	s.LightSleep = row.LightSleep
+	s.RemSleep = row.LightSleep
+	s.CreatedTimestamp = row.CreatedTimestamp
+	s.UpdatedTimestamp = row.UpdatedTimestamp
+}
+
+func (s *Sleep) PopulateFromDbSleep(dbSleep austinapi_db.Sleep) {
+	s.ID, _ = IdHasher.Encode([]uint64{uint64(dbSleep.ID)})
+	s.Date = dbSleep.Date
+	s.Rating = dbSleep.Rating
+	s.TotalSleep = dbSleep.TotalSleep
+	s.DeepSleep = dbSleep.DeepSleep
+	s.LightSleep = dbSleep.LightSleep
+	s.RemSleep = dbSleep.RemSleep
+	s.CreatedTimestamp = dbSleep.CreatedTimestamp
+	s.UpdatedTimestamp = dbSleep.UpdatedTimestamp
 }
 
 func GetNextToken(sleeps []austinapi_db.SleepsRow) string {
@@ -89,60 +142,57 @@ func handleError(w http.ResponseWriter, statusCode int, message string) {
 	json.NewEncoder(w).Encode(GenericMessage{Message: message})
 }
 
-// TODO fix GetSleep to use sqlid
 func (h *SleepHandler) GetSleep(w http.ResponseWriter, r *http.Request) {
-	//sleepIdMatches := SleepRgxId.FindStringSubmatch(r.URL.String())
-	//
-	//if len(sleepIdMatches) < 2 {
-	//	ErrorLog.Printf("error regex parsing url '%s' with regex '%s'", r.URL.Path, SleepRgxId.String())
-	//	handleError(w, http.StatusInternalServerError, "Issue parsing specified id")
-	//	return
-	//}
-	//
-	//connStr := GetDatabaseConnectionString()
-	//ctx := context.Background()
-	//
-	//conn, err := pgx.Connect(ctx, connStr)
-	//if err != nil {
-	//	ErrorLog.Printf("DB Connection error: %v", err)
-	//	handleError(w, http.StatusInternalServerError, "Internal Error")
-	//	return
-	//}
-	//defer conn.Close(ctx)
-	//
-	//apiDb := austinapi_db.New(conn)
-	//
-	//sleepUuid, err := uuid.Parse(sleepIdMatches[1])
-	//if err != nil {
-	//	ErrorLog.Printf("error parsing UUID from http request: %v", err)
-	//	handleError(w, http.StatusInternalServerError, fmt.Sprintf("Unable to parse specified id: %s", sleepIdMatches[1]))
-	//	return
-	//}
-	//
-	//getSleepResult, err := apiDb.GetSleep(ctx, sleepUuid)
-	//
-	//if err != nil {
-	//	ErrorLog.Printf("error retrieving sleep with id '%s': %v", sleepUuid.String(), err)
-	//	handleError(w, http.StatusInternalServerError, "Internal Error")
-	//	return
-	//}
-	//
-	//if len(getSleepResult) != 1 {
-	//	InfoLog.Printf("sleep with id '%s' was not found in database", sleepUuid.String())
-	//	handleError(w, http.StatusNotFound, fmt.Sprintf("Sleep not found with id %s", sleepUuid.String()))
-	//	return
-	//}
-	//
-	//result := getSleepResult[0]
-	//jsonBytes, err := json.Marshal(result)
-	//if err != nil {
-	//	ErrorLog.Printf("error marshaling JSON response: %v", err)
-	//	handleError(w, http.StatusInternalServerError, "Internal Error")
-	//	return
-	//}
-	//
-	//w.WriteHeader(http.StatusOK)
-	//w.Write(jsonBytes)
+	sleepIdMatches := SleepRgxId.FindStringSubmatch(r.URL.String())
+
+	if len(sleepIdMatches) < 2 {
+		ErrorLog.Printf("error regex parsing url '%s' with regex '%s'", r.URL.Path, SleepRgxId.String())
+		handleError(w, http.StatusInternalServerError, "Issue parsing specified id")
+		return
+	}
+
+	sqid := sleepIdMatches[1]
+
+	connStr := GetDatabaseConnectionString()
+	ctx := context.Background()
+
+	conn, err := pgx.Connect(ctx, connStr)
+	if err != nil {
+		ErrorLog.Printf("DB Connection error: %v", err)
+		handleError(w, http.StatusInternalServerError, "Internal Error")
+		return
+	}
+	defer conn.Close(ctx)
+
+	apiDb := austinapi_db.New(conn)
+
+	idFromSqid := GetIdFromToken(sqid)
+
+	getSleepResult, err := apiDb.GetSleep(ctx, idFromSqid)
+
+	if err != nil {
+		ErrorLog.Printf("error retrieving sleep with id '%v': %v", idFromSqid, err)
+		handleError(w, http.StatusInternalServerError, "Internal Error")
+		return
+	}
+
+	if len(getSleepResult) != 1 {
+		InfoLog.Printf("sleep with id '%v was not found in database", idFromSqid)
+		handleError(w, http.StatusNotFound, fmt.Sprintf("Sleep not found with id %s", sqid))
+		return
+	}
+
+	sleep := Sleep{}
+	sleep.PopulateFromDbSleep(getSleepResult[0])
+	jsonBytes, err := json.Marshal(sleep)
+	if err != nil {
+		ErrorLog.Printf("error marshaling JSON response: %v", err)
+		handleError(w, http.StatusInternalServerError, "Internal Error")
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(jsonBytes)
 }
 
 func (h *SleepHandler) ListSleep(w http.ResponseWriter, r *http.Request) {
@@ -189,7 +239,7 @@ func (h *SleepHandler) ListSleep(w http.ResponseWriter, r *http.Request) {
 	}
 
 	sleeps := Sleeps{}
-	sleeps.Data = sleepsFromDb
+	sleeps.Data = ConvertSleepsRow(sleepsFromDb)
 	sleeps.NextToken = GetNextToken(sleepsFromDb)
 	sleeps.PreviousToken = GetPreviousToken(sleepsFromDb)
 
